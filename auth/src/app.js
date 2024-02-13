9554cbee95fd62fc29dac083ee8eecbe8d915cd5
marijoyapa <marijoy.apa@tooltwist.com>
2024-02-13 23:30:11 +0800

    Update app.js

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,15 @@
 import React, { Component, } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Button, CardSection, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
-import { initializeApp } from 'firebase/app';
+import { initializeApp, } from 'firebase/app';
+import { onAuthStateChanged, getAuth, signOut } from 'firebase/auth';
 
 
 class App extends Component {
+    state = { loggedIn: null }
+
     UNSAFE_componentWillMount() {
         console.log('hello word')
         initializeApp({
@@ -19,6 +22,40 @@ class App extends Component {
 
         })
 
+        const auth = getAuth();
+
+        auth.onAuthStateChanged((user) => {
+            console.log('user is ', user)
+            if (user) {
+                this.setState({ loggedIn: true })
+            }
+            else {
+                this.setState({ loggedIn: false })
+
+            }
+
+        })
+
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => {
+                            const auth = getAuth();
+
+                            signOut(auth)
+                        }}>Logout</Button>
+                    </CardSection>
+                )
+            case false:
+                return <LoginForm />
+
+            default:
+                return <Spinner />
+        }
     }
 
 
@@ -26,9 +63,10 @@ class App extends Component {
 
     render() {
         return (
-            <View>
+            <View >
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
+
             </View>
         )
     }
